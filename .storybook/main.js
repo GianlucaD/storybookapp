85490ef18d6d0a1c7da748ffe4761aabe63c37e4
Gianluca Daffre <gianluca.daffre@noseryoung.ch>
2026-01-22 08:47:56 +0100

    refresh

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,28 +1,20 @@
-module.exports = {
-  stories: ['../src/**/*.stories.js'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-actions/register',
-    {
-      name: '@storybook/addon-storysource',
-      options: {
-        configureJSX: true,
-      },
-    },
-    '@storybook/addon-viewport/register',
-    '@storybook/addon-backgrounds/register',
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-a11y/register',
-    {
-      name : '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-        babelOptions: {},
-        sourceLoaderOptions: null,
-      },
-    }
+
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  
+  "addons": [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": "@storybook/react-webpack5",
+  "staticDirs": [
+    "../public"
+  ]
 };
+export default config;
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,24 +1,10 @@
-import { addons } from '@storybook/addons';
-import { themes } from '@storybook/theming';
-import { create } from "@storybook/theming";
+import { addons } from 'storybook/manager-api';
+import { create } from 'storybook/theming';
 
-
-// test me
 addons.setConfig({
     theme: create({
         base: "dark",
         brandTitle: "My Storybook",
         brandImage: "logo192.png"
-    }),
-    backgrounds: [
-        { name: "white", value: "#fff" },
-        { name: "ligh grey", value: "#CDCDCD" },
-        { name: "grey", value: "#888" },
-        { name: "dark grey", value: "#2f2f2f" },
-        { name: "black", value: "#000" },
-        { name: "twitter", value: "#00aced" },
-        { name: "facebook", value: "#3b5998" },
-        { name: "noser", value: "#3b76d9" }
-    ]
+    })
 });
-
